dfdfb1b94eb9b9b654f12ed5958a4bb53ca0742a
Kacper Bojanowski <72980071+kcbojanowski@users.noreply.github.com>
2024-12-22 13:55:50 +0100

    Update frontend/app/dashboard/overview/_components/overview.tsx
    Co-authored-by: Piotr Ciesielczyk <piotrciesielczyk2001@gmail.com>

--- frontend/app/dashboard/overview/_components/overview.tsx
+++ frontend/app/dashboard/overview/_components/overview.tsx
@@ -130,7 +130,7 @@ export default function OverViewPage() {
               <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle className="text-sm font-medium">
-                    Avarage Progress
+                    Average Progress
                   </CardTitle>
                   <AvarageProgressIcon></AvarageProgressIcon>
                 </CardHeader>
