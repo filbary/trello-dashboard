957c46e398642ffc2c835857ce88413ae85afe74
Kacper Bojanowski <72980071+kcbojanowski@users.noreply.github.com>
2024-12-22 13:55:55 +0100

    Update frontend/app/dashboard/overview/_components/overview.tsx
    Co-authored-by: Piotr Ciesielczyk <piotrciesielczyk2001@gmail.com>

--- frontend/app/dashboard/overview/_components/overview.tsx
+++ frontend/app/dashboard/overview/_components/overview.tsx
@@ -119,7 +119,7 @@ export default function OverViewPage() {
               <Card>
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                   <CardTitle className="text-sm font-medium">
-                    Total Cards
+                    Total Active Cards
                   </CardTitle>
                   <TotalCardsIcon></TotalCardsIcon>
                 </CardHeader>
