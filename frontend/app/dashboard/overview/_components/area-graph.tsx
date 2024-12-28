ae8e18fa5b49e357fd9b5b2c10cd3c8271da9b2d
kcbojanowski <k1bojanowski@gmail.com>
2024-12-28 17:12:30 +0100

    labels update for charts

--- frontend/app/dashboard/overview/_components/area-graph.tsx
+++ frontend/app/dashboard/overview/_components/area-graph.tsx
@@ -17,6 +17,7 @@ import {
   ChartTooltip,
   ChartTooltipContent
 } from '@/components/ui/chart';
+import * as React from "react";
 
 const chartConfig = {
   frontend: {
@@ -51,71 +52,65 @@ export function AreaGraph({ chartData }: AreaGraphProps) {
       </CardHeader>
       <CardContent>
         <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[310px] w-full"
+            config={chartConfig}
+            className="aspect-auto h-[310px] w-full"
         >
           <AreaChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              left: 20,
-              right: 20
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => {
-                const date = new Date(value);
-                return date.toLocaleDateString('en-US', {
-                  month: 'short',
-                  day: 'numeric'
-                });
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 20,
+                right: 20
               }}
+          >
+            <CartesianGrid vertical={false}/>
+            <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => {
+                  const date = new Date(value);
+                  return date.toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric'
+                  });
+                }}
             />
             <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="dot" />}
+                cursor={false}
+                content={<ChartTooltipContent indicator="dot"/>}
             />
             <Area
-              dataKey="frontend"
-              type="monotone"
-              fill="var(--color-frontend)"
-              fillOpacity={0.4}
-              stroke="var(--color-frontend)"
-              stackId="a"
+                dataKey="frontend"
+                type="monotone"
+                fill="var(--color-frontend)"
+                fillOpacity={0.4}
+                stroke="var(--color-frontend)"
+                stackId="a"
             />
             <Area
-              dataKey="backend"
-              type="monotone"
-              fill="var(--color-backend)"
-              fillOpacity={0.4}
-              stroke="var(--color-backend)"
-              stackId="a"
+                dataKey="backend"
+                type="monotone"
+                fill="var(--color-backend)"
+                fillOpacity={0.4}
+                stroke="var(--color-backend)"
+                stackId="a"
             />
             <Area
-              dataKey="devops"
-              type="monotone"
-              fill="var(--color-devops)"
-              fillOpacity={0.4}
-              stroke="var(--color-devops)"
-              stackId="a"
+                dataKey="devops"
+                type="monotone"
+                fill="var(--color-devops)"
+                fillOpacity={0.4}
+                stroke="var(--color-devops)"
+                stackId="a"
             />
           </AreaChart>
         </ChartContainer>
-      </CardContent>
-      <CardFooter>
-        <div className="flex w-full items-start gap-2 text-sm">
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2 font-medium leading-none">
-              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-            </div>
-          </div>
+        <div className="leading-none text-muted-foreground text-sm">
+          Showing the distribution of task types
         </div>
-      </CardFooter>
+      </CardContent>
     </Card>
   );
 }
--- frontend/app/dashboard/overview/_components/pie-graph.tsx
+++ frontend/app/dashboard/overview/_components/pie-graph.tsx
@@ -116,9 +116,6 @@ export function PieGraph({ chartData }: PieGraphProps) {
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
         <div className="leading-none text-muted-foreground">
           Showing the distribution of tasks across all tables
         </div>
